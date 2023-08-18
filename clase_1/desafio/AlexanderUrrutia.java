efe1a6bd30a512632c939710606f499d1fea0416
Alexander Urrutia <aurrutia.externo@coopeuch.cl>
2023-08-18 08:08:03 -0400

    feat: :art: Entregable desafio numero 1 finalizado
    Se realiza el entregable de coderhouse, para mostrar un numero tipo INT y FLOAT

---
+++ clase_1/desafio/AlexanderUrrutia.java
@@ -0,0 +1,12 @@
+package clase_1.desafio;
+
+public class AlexanderUrrutia {
+
+     public static void main(String[] args) {
+          int stock = 20;
+          float stockFloat = stock;
+
+          System.out.println("Numero entero:" + stock);
+          System.out.println("Numero en formato float:" + stockFloat);
+     }
+}
