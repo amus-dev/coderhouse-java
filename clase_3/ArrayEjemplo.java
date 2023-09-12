cdda06c408f48e651ea6d9a98a64439b8a555353
Alexander Urrutia <aurrutia.externo@coopeuch.cl>
2023-09-12 17:37:36 -0300

    Avances

---
+++ clase_3/ArrayEjemplo.java
@@ -0,0 +1,9 @@
+package clase_3;
+
+public class ArrayEjemplo {
+     public static void main(String[] args) {
+          int [] edades = {23,22,30};
+
+          System.out.println("La primera edad es: " + edades[0]);
+     }
+}
---
+++ clase_3/ArrayListEjemplo.java
@@ -0,0 +1,23 @@
+package clase_3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEjemplo {
+     public static void main(String[] args) {
+
+          List<String> coloresOriginal = new ArrayList<>();
+          coloresOriginal.add("Blanco");
+          coloresOriginal.add("Azul");
+
+          // List<String> coloresDestino = new ArrayList<>();
+          // coloresDestino.add("Celeste");
+          // coloresDestino.add("Verde");
+          // coloresDestino.add("Blanco");
+
+          // // Métodos
+          // // containsAll
+          // boolean contieneOriginal = coloresOriginal.containsAll(coloresDestino);
+          // System.out.println("Contiene original: " + contieneOriginal);
+     }
+}
