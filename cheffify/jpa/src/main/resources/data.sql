06bcbeaaeda1e7f16e5392497685ae17f2fc5fb4
Matias <matias04041994@gmail.com>
2023-09-19 18:29:11 -0300

    feat: :art: Inicializar proyecto libros cheffify
    Se inicia el proyecto desafio final para coderhouse, sera un proyecto relacionado con libros y autores para consumir la API de libros desde el front

---
+++ cheffify/jpa/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+/* Insertar productos por defecto al iniciar proyecto */
+INSERT INTO AUTHORS (name) values
+('Paulo Coelho'),
+('GUZMAN CARLOS');
+
+INSERT INTO BOOKS (title, price, year_book, cover) values
+('Esteban Paredes. El Elegido', 38170, 2023, 'https://feriachilenadellibro.cl/wp-content/uploads/2023/04/9789560907073.20230413111318-1.jpg'),
+('El camino del arquero', 15200, 2023, 'https://feriachilenadellibro.cl/wp-content/uploads/2023/09/9789562586351.20230810140328-15.png'),
+('Once Minutos', 13900, 2022, 'https://feriachilenadellibro.cl/wp-content/uploads/2023/05/9789566165590.jpg');
